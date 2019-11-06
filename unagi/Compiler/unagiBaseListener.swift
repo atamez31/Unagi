1ba07e96d2220f1d1d21683e351f89287be53203
alexelmtz <alexelmtz@outlook.com>
2019-11-06 14:18:17 -0600

    Generate quadruples for arithmetic operations

--- unagi/Compiler/unagiBaseListener.swift
+++ unagi/Compiler/unagiBaseListener.swift
@@ -15,6 +15,7 @@ open class unagiBaseListener: unagiListener {
     var semanticCube = SemanticCube.init()
 
     var varTable = VarTable()
+    var constTable: [String: Var] = [:]
 
     var POper = [String]()
     var PSaltos = [Int]()
@@ -28,22 +29,28 @@ open class unagiBaseListener: unagiListener {
 
     var globalMemory = Memory.init(realMemorySpace: 0)
     var localMemory = Memory.init(realMemorySpace: 10000)
+    var constantMemory = Memory.init(realMemorySpace: 20000)
+
      public init() { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	open func enterProgram(_ ctx: unagiParser.ProgramContext) {
-        quads.append(Quadruple.init(op: "GOTO", leftVal: -1, rightVal: -1, result: -1))
-        PSaltos.append(quads.count-1)
-    }
+  open func enterProgram(_ ctx: unagiParser.ProgramContext) {
+    quads.append(Quadruple.init(op: "GOTO", leftVal: -1, rightVal: -1, result: -1))
+    PSaltos.append(quads.count-1)
+  }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	open func exitProgram(_ ctx: unagiParser.ProgramContext) { }
+	open func exitProgram(_ ctx: unagiParser.ProgramContext) {
+    for quad in quads {
+      print(quad.op + " " + quad.leftVal + " " + quad.rightVal + " " + quad.result)
+    }
+  }
 
 	/**
 	 * {@inheritDoc}
@@ -252,7 +259,31 @@ open class unagiBaseListener: unagiListener {
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	open func exitTerm(_ ctx: unagiParser.TermContext) { }
+  open func exitTerm(_ ctx: unagiParser.TermContext) {
+    if POper.last == "+" || POper.last == "-" {
+      let op = POper.popLast()!
+      let resultType = semanticCube.validateOperation(op: op, leftOp: PTypes.popLast()!, rightOp: PTypes.popLast()!)
+      if  resultType == Type.none {
+        // TODO: Throw an error for incorrect operation.
+      }
+      let opRight = PilaO.popLast()!
+      let opLeft = PilaO.popLast()!
+      let tempAddress = localMemory.getNextAddress(type: resultType)
+      let quad = Quadruple.init(op: op, leftVal: opLeft, rightVal: opRight, result: tempAddress)
+      quads.append(quad)
+      PTypes.append(resultType)
+      PilaO.append(tempAddress)
+    }
+
+    if let parent = ctx.parent as? unagiParser.ExpContext {
+      if let sum = parent.SUM()?.getText() {
+        POper.append(sum)
+      }
+      if let sub = parent.SUB()?.getText() {
+        POper.append(sub)
+      }
+    }
+  }
 
 	/**
 	 * {@inheritDoc}
@@ -265,7 +296,38 @@ open class unagiBaseListener: unagiListener {
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	open func exitFactor(_ ctx: unagiParser.FactorContext) { }
+  open func exitFactor(_ ctx: unagiParser.FactorContext) {
+    if let id = ctx.ID()?.getText() {
+      if let variable = varTable.getDictFunc(name: scope)?.getVariable(name: id) {
+        PTypes.append(variable.type)
+        PilaO.append(variable.memory_address)
+      }
+    }
+
+    if POper.last == "*" || POper.last == "/" {
+      let op = POper.popLast()!
+      let resultType = semanticCube.validateOperation(op: op, leftOp: PTypes.popLast()!, rightOp: PTypes.popLast()!)
+      if  resultType == Type.none {
+        // TODO: Throw an error for incorrect operation.
+      }
+      let opRight = PilaO.popLast()!
+      let opLeft = PilaO.popLast()!
+      let tempAddress = localMemory.getNextAddress(type: resultType)
+      let quad = Quadruple.init(op: op, leftVal: opLeft, rightVal: opRight, result: tempAddress)
+      quads.append(quad)
+      PTypes.append(resultType)
+      PilaO.append(tempAddress)
+    }
+
+    if let parent = ctx.parent as? unagiParser.TermContext {
+      if let mult = parent.MULT()?.getText() {
+        POper.append(mult)
+      }
+      if let div = parent.DIV()?.getText() {
+        POper.append(div)
+      }
+    }
+  }
 
 	/**
 	 * {@inheritDoc}
@@ -278,7 +340,77 @@ open class unagiBaseListener: unagiListener {
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	open func exitConstant(_ ctx: unagiParser.ConstantContext) { }
+  open func exitConstant(_ ctx: unagiParser.ConstantContext) {
+    if let constant = ctx.CTE_N()?.getText() {
+      if let constVar = constTable[constant] {
+        PilaO.append(constVar.memory_address)
+        PTypes.append(constVar.type)
+      } else {
+        let address = constantMemory.getNextAddress(type: Type.num)
+        let variable = Var.init(name: constant, type: Type.num, memory_address: address)
+        constTable[constant] = variable
+        PilaO.append(address)
+        PTypes.append(Type.num)
+      }
+    } else if let constant = ctx.CTE_D()?.getText() {
+        if let constVar = constTable[constant] {
+          PilaO.append(constVar.memory_address)
+          PTypes.append(constVar.type)
+        } else {
+          let address = constantMemory.getNextAddress(type: Type.decimal)
+          let variable = Var.init(name: constant, type: Type.decimal, memory_address: address)
+          constTable[constant] = variable
+          PilaO.append(address)
+          PTypes.append(Type.decimal)
+        }
+    } else if let constant = ctx.CTE_C()?.getText() {
+        if let constVar = constTable[constant] {
+          PilaO.append(constVar.memory_address)
+          PTypes.append(constVar.type)
+        } else {
+          let address = constantMemory.getNextAddress(type: Type.char)
+          let variable = Var.init(name: constant, type: Type.char, memory_address: address)
+          constTable[constant] = variable
+          PilaO.append(address)
+          PTypes.append(Type.char)
+        }
+    } else if let constant = ctx.CTE_P()?.getText() {
+        if let constVar = constTable[constant] {
+          PilaO.append(constVar.memory_address)
+          PTypes.append(constVar.type)
+        } else {
+          let address = constantMemory.getNextAddress(type: Type.phrase)
+          let variable = Var.init(name: constant, type: Type.phrase, memory_address: address)
+          constTable[constant] = variable
+          PilaO.append(address)
+          PTypes.append(Type.phrase)
+        }
+    } else {
+      if ctx.getText() == "true" {
+        if let constVar = constTable["true"] {
+          PilaO.append(constVar.memory_address)
+          PTypes.append(constVar.type)
+        } else {
+          let address = constantMemory.getNextAddress(type: Type.num)
+          let variable = Var.init(name: "true", type: Type.num, memory_address: address)
+          constTable["true"] = variable
+          PilaO.append(address)
+          PTypes.append(Type.num)
+        }
+      } else {
+          if let constVar = constTable["false"] {
+            PilaO.append(constVar.memory_address)
+            PTypes.append(constVar.type)
+          } else {
+            let address = constantMemory.getNextAddress(type: Type.num)
+            let variable = Var.init(name: "false", type: Type.num, memory_address: address)
+            constTable["false"] = variable
+            PilaO.append(address)
+            PTypes.append(Type.num)
+          }
+      }
+    }
+  }
 
 	/**
 	 * {@inheritDoc}
